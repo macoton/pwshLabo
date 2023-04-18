cd5870f5916d10c71c524d8c223d0caf969d6c4d
Admini strator <macoton@104.net>
2023-04-19 03:02:23 +0900

    changeAdminRunScriptTest

---
+++ changeAdmin/changeAdminRunScriptTest_OK.ps1
@@ -0,0 +1,8 @@
+. .\changeAdmin\changeAdminRunScript.ps1
+ChangeAdminRunScript
+Write-Host ('$pwd.Path: {0}' -f $pwd.Path)
+Read-Host
+Set-Location '.\changeAdmin'
+Write-Host ('$pwd.Path: {0}' -f $pwd.Path)
+Read-Host
+Set-Location '..'
